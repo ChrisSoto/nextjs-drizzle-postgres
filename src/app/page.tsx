37b5410cb425ad1c3dbf927b4b0e5ad549f04063
chris soto <christopherrsoto@gmail.com>
2024-03-11 10:53:45 -0700

    nothing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <div>
       Connected to database?
-      <p>{count.length}</p>
+      <p>{count.length/2}</p>
     </div>
   );
 }
--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 
-const migrationClient = postgres(process.env.POSTGRES_URL!, { max: 1 });
+const migrationClient = postgres(process.env.POSTGRES_URL!, { max: 1, onnotice: () => {}});
 migrate(drizzle(migrationClient), { migrationsFolder: 'src/migrations' });
 const queryClient = postgres(process.env.POSTGRES_URL!);
 export const db = drizzle(queryClient)
